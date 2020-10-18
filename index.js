747fdbb1052a7b22113b32624d3382b322785d60
ryokkkke <rrrtechrrr@gmail.com>
2020-10-18 18:47:03 +0900

    prettierによる修正

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-const { getOptions } = require('loader-utils')
-const readingTime = require('reading-time')
-const emoji = require('remark-emoji')
-const images = require('remark-images')
-const textr = require('remark-textr')
-const slug = require('remark-slug')
-const breaks = require('remark-breaks')
-const mdx = require('@mdx-js/mdx')
-const mdxTableOfContents = require('mdx-table-of-contents')
-const mdxExportJSONByDefault = require('mdx-constant')
-const grayMatter = require('gray-matter')
-const typography = require('./typography')
-const rehypePrism = require('./prism')
+const { getOptions } = require("loader-utils");
+const readingTime = require("reading-time");
+const emoji = require("remark-emoji");
+const images = require("remark-images");
+const textr = require("remark-textr");
+const slug = require("remark-slug");
+const breaks = require("remark-breaks");
+const mdx = require("@mdx-js/mdx");
+const mdxTableOfContents = require("mdx-table-of-contents");
+const mdxExportJSONByDefault = require("mdx-constant");
+const grayMatter = require("gray-matter");
+const typography = require("./typography");
+const rehypePrism = require("./prism");
 
-module.exports = async function(source) {
-  let result
+module.exports = async function (source) {
+  let result;
   const { data, content: mdxContent } = grayMatter(source);
-  const callback = this.async()
+  const callback = this.async();
   const options = Object.assign(
     {
       remarkPlugins: [
         slug,
-        images, 
+        images,
         emoji,
         breaks,
-        [textr, { plugins: [typography] }]
+        [textr, { plugins: [typography] }],
       ],
-      rehypePlugins: [
-        rehypePrism,
-      ],
+      rehypePlugins: [rehypePrism],
       compilers: [
         mdxTableOfContents,
-        mdxExportJSONByDefault('frontMatter', data)
-      ]
+        mdxExportJSONByDefault("frontMatter", data),
+      ],
     },
     getOptions(this),
-    {filepath: this.resourcePath}
+    { filepath: this.resourcePath }
   );
 
   try {
-    result = await mdx(mdxContent, options)
-  } catch(err) {
-    return callback(err)
+    result = await mdx(mdxContent, options);
+  } catch (err) {
+    return callback(err);
   }
 
-  const estimatedReadingTime = readingTime(source)
+  const estimatedReadingTime = readingTime(source);
 
   let code = `
 import React from 'react'
 import { mdx } from '@mdx-js/react'
 export const readingTime = ${JSON.stringify(estimatedReadingTime)}
 ${result}
-`
+`;
 
-  return callback(null, code)
-}
+  return callback(null, code);
+};
