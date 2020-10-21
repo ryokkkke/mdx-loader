03f3483a206b7936d3b9025eb8e390abf0707ccf
ryokkkke <rrrtechrrr@gmail.com>
2020-10-21 23:29:36 +0900

    更新

--- table-of-contents/index.js
+++ table-of-contents/index.js
@@ -86,7 +86,7 @@ const codelifyTreeInfo = (tableOfContents) => {
     .map(codeifyTreeInfoItem)
     .join(",");
 
-  return `\nexport const tableOfContents = [${tableOfContentsCodes}];`;
+  return `\nexport const tableOfContents = (components={}) => [${tableOfContentsCodes}];`;
 };
 
 // for an item options.rehypePlugins of mdx function of @mdx-js/mdx
