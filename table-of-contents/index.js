de4f90d51ecc72ce28a6c5caf9f9905ac15ba336
ryokkkke <rrrtechrrr@gmail.com>
2020-10-21 22:10:51 +0900

    修正

--- table-of-contents/index.js
+++ table-of-contents/index.js
@@ -1,50 +1,26 @@
 const toJSX = require("@mdx-js/mdx/mdx-hast-to-jsx").toJSX;
 
-module.exports = () => (astTree) => {
-  console.log(astTree);
-  if (astTree == undefined) {
-    console.error("astTree is undefined");
-    return;
+function toFragment(nodes) {
+  if (nodes.length === 1 && nodes[0].type === "text") {
+    return JSON.stringify(nodes[0].value);
+  } else {
+    return "<React.Fragment>" + nodes.map(toJSX).join("") + "</React.Fragment>";
   }
+}
 
-  const children = astTree.children;
-  if (children == undefined) {
-    console.error("children is undefined");
-    return;
-  }
-  if (children.length === 0) {
-    console.error("children is empty");
-    return;
-  }
+const tableOfContentsListSerializer = (nodes) =>
+  nodes.map((node) => tableOfContentsNodeSerializer(node));
 
-  astTree.children = children.concat([
-    /* info.tableOfContents */
-  ]);
-  return astTree;
-};
+function tableOfContentsNodeSerializer(node) {
+  return {
+    id: JSON.stringify(node.id),
+    level: node.level,
+    title: node.title,
+    children: tableOfContentsListSerializer(node.children),
+  };
+}
 
-// function mdxTableOfContents(options = {}) {
-//   let OldCompiler = this.Compiler;
-//   let info;
-
-//   this.Compiler = (tree) => {
-//     let code = OldCompiler(tree, {}, options);
-
-//     if (!info.hasTableOfContentsExport) {
-//       code += `\nexport const tableOfContents = (components={}) => ${tableOfContentsListSerializer(
-//         info.tableOfContents
-//       )}\n`;
-//     }
-
-//     return code;
-//   };
-
-//   return function transformer(node) {
-//     info = getInfo(node, options);
-//   };
-// }
-
-function getInfo(
+function scanAstTree(
   root,
   { minTableOfContentsLevel = 1, maxTableOfContentsLevel = 3 } = {}
 ) {
@@ -57,7 +33,7 @@ function getInfo(
   const tableOfContentsIds = {};
 
   root.children.forEach((node) => {
-    // export tableOfContents
+    // whether node has "export const tableOfContents" or not
     if (
       node.type === "export" &&
       node.value.indexOf("tableOfContents") !== -1
@@ -65,59 +41,42 @@ function getInfo(
       info.hasTableOfContentsExport = true;
     }
 
-    //
-    if (node.type === "element" && /^h\d$/.test(node.tagName)) {
-      const level = parseInt(node.tagName[1]);
-      if (
-        level >= minTableOfContentsLevel &&
-        level <= maxTableOfContentsLevel
-      ) {
-        const id = node.properties.id;
-        levelIds[level - 1] = id;
-        const parent = tableOfContentsIds[levelIds[level - 2]];
-        const item = {
-          id,
-          level,
-          title: toFragment(node.children),
-          children: [],
-        };
-        if (parent) {
-          parent.children.push(item);
-        } else {
-          info.tableOfContents.push(item);
-        }
-        tableOfContentsIds[id] = item;
-      }
+    if (node.type !== "element") return;
+    if (!/^h\d$/.test(node.tagName)) return;
+
+    const level = parseInt(node.tagName[1]);
+    if (level < minTableOfContentsLevel) return;
+    if (level > maxTableOfContentsLevel) return;
+
+    const id = node.properties.id;
+    levelIds[level - 1] = id;
+    const parent = tableOfContentsIds[levelIds[level - 2]];
+    const item = {
+      id,
+      level,
+      title: toFragment(node.children),
+      children: [],
+    };
+    if (parent) {
+      parent.children.push(item);
+    } else {
+      info.tableOfContents.push(item);
     }
+    tableOfContentsIds[id] = item;
   });
 
   return info;
 }
 
-function toFragment(nodes) {
-  if (nodes.length === 1 && nodes[0].type === "text") {
-    return JSON.stringify(nodes[0].value);
-  } else {
-    return "<React.Fragment>" + nodes.map(toJSX).join("") + "</React.Fragment>";
-  }
-}
+// for an item options.rehypePlugins of mdx function of @mdx-js/mdx
+module.exports = () => (astTree) => {
+  const children = astTree.children;
+  if (children == undefined) return;
+  if (children.length === 0) return;
 
-const tableOfContentsListSerializer = (nodes) =>
-  nodes.map((node) => tableOfContentsNodeSerializer(node));
+  const treeinfo = scanAstTree(astTree);
+  if (treeinfo.hasTableOfContentsExport) return astTree;
 
-const tableOfContentsNodeSerializer = (node) => ({
-  id: JSON.stringify(node.id),
-  level: node.level,
-  title: node.title,
-  children: tableOfContentsListSerializer(node.children),
-});
-
-// function indentString(size, string) {
-//   return string
-//     .split("\n")
-//     .map((x) => " ".repeat(size) + x)
-//     .join("\n")
-//     .trim();
-// }
-
-// module.exports = mdxTableOfContents;
+  astTree.children = children.concat([treeinfo.tableOfContents]);
+  return astTree;
+};
